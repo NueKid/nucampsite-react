4974ed41faa20e00c5e743d964fffd2a6d784f67
Nuekid <ryen.masters@gmail.com>
2024-12-08 13:00:35 -0800

    added Campsite Cards, Header, and Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import CampsitesList from './features/campsites/CampsitesList.js';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello nucamp!
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
