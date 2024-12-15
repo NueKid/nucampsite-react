35424847a184294f00d5593a84feb599a2070dd2
Nuekid <ryen.masters@gmail.com>
2024-12-15 00:17:11 -0800

    added pages for campsite detail with comments section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React from 'react';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
+import CampsiteDetailPage from './pages/CampsiteDetailPage.js';
 import './App.css';
 
 function App() {
@@ -14,7 +15,11 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />}/>
-        <Route path='directory' element={<CampsitesDirectoryPage />}/>
+        <Route path='directory' element={<CampsitesDirectoryPage />}/>'
+        <Route 
+          path='directory/:campsiteId'
+          element={<CampsiteDetailPage />}
+        />
       </Routes>
       <Footer />
     </div>
